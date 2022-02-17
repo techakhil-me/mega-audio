beb40f9697573eb886f09beacc98afeb1e89d4ec
TechAkhil <43501564+techakhil-me@users.noreply.github.com>
2022-02-17 10:17:09 +0530

    meta tags updated

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+// loading
+document.querySelector("img").onload = () =>
+  setTimeout(() => {
+    let loader = document.querySelector(".loading");
+    loader.classList.toggle("complete");
+    loader.addEventListener(
+      "transitionend",
+      () => {
+        loader.classList.toggle("remove-loading");
+      },
+      false
+    );
+  }, 2000);
+
 //  toggle navbar
 function toggleMenu(el) {
   el.classList.toggle("open");
@@ -7,7 +21,7 @@ function toggleMenu(el) {
 
 // toggle accordian
 function toggleAccordian(el) {
-  console.log()
-  el.firstElementChild.classList.toggle("minus")
+  console.log();
+  el.firstElementChild.classList.toggle("minus");
   el.parentElement.parentElement.classList.toggle("accordian-open");
 }
