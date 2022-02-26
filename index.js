9da13707d69e625eeb6d48af7e3c413b66778fea
TechAkhil <43501564+techakhil-me@users.noreply.github.com>
2022-02-26 23:02:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,16 +11,3 @@ setTimeout(() => {
     );
   }, 3000);
 
-//  toggle navbar
-function toggleMenu(el) {
-  el.classList.toggle("open");
-  document.querySelector(".openmenu").classList.toggle("closemenu");
-  document.querySelector("nav").classList.toggle("invert");
-}
-
-// toggle accordian
-function toggleAccordian(el) {
-  console.log();
-  el.firstElementChild.classList.toggle("minus");
-  el.parentElement.parentElement.classList.toggle("accordian-open");
-}
